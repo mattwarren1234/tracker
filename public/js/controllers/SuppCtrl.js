1430124016050edace6425366edac3be92830a34
Matt Warren <mattwarrenmail@gmail.com>
2014-08-22 22:39:43 -0700

    removed unused variables; add benefit only adds if item is unique

--- public/js/controllers/SuppCtrl.js
+++ public/js/controllers/SuppCtrl.js
@@ -3,35 +3,29 @@ angular.module('SuppCtrl', [])
         $scope.itemToEdit = {};
         $scope.selected = {};
         $scope.windowMessage = {
-                text : ""
+            text: ""
         };
         $scope.object = {item1: "ohhai"};
         $scope.showInitialEmptyItem = false;
-
         $scope.initSupp = {};
         $scope.defaultValues = {
             name: "New Supplement",
             description: "",
             dosage: "",
-            benefits: [{text : ""}]
+            benefits: []
         };
         Object.freeze($scope.defaultValues);
-
         $scope.showInitial = function() {
             $scope.initSupp.name = $scope.defaultValues.name;
             $scope.initSupp.description = $scope.defaultValues.description;
             $scope.initSupp.dosage = $scope.defaultValues.dosage;
             $scope.initSupp.benefits = $scope.defaultValues.benefits;
             $scope.showInitialEmptyItem = true;
-
         };
-
         $scope.addSupp = function() {
             $scope.showInitial();
             $scope.itemToEdit = $scope.initSupp;
         };
-
-
         $scope.refreshList = function() {
             Supps.get()
                 .success(function(data) {
@@ -40,7 +34,6 @@ angular.module('SuppCtrl', [])
                     $scope.itemToEdit = {};
                 });
         };
-
         $scope.setEdit = function(item) {
             console.log("set edit called");
             if (item === $scope.itemToEdit)
@@ -49,18 +42,16 @@ angular.module('SuppCtrl', [])
             $scope.itemToEdit = item;
             $scope.titleEditMode = false;
         };
-
         $scope.refreshList();
-
     })
     .directive("suppEditable", ['Supps',
         function(Supps) {
             return{
                 templateUrl: '/public/views/editable.html',
                 controller: function($scope) {
-                    //     $scope.obj.item1 = " try try ";
-//               $scope.obj = $scope.$parent.obj;
-                    $scope.newBenefit = "";
+                    $scope.newBenefit = {
+                        description: ""
+                    }
                     $scope.titleEditMode = false;
                     $scope.toggleTitleEdit = function() {
                         $scope.titleEditMode = !$scope.titleEditMode;
@@ -68,14 +59,18 @@ angular.module('SuppCtrl', [])
                     $scope.saveTitle = function() {
                         $scope.titleEditMode = false;
                     };
+                    $scope.notAlreadySaved = function(currBenefit) {
+                        return currBenefit.description !== $scope.newBenefit.description;
+                    };
                     $scope.addBenefit = function() {
-                        if (!$.isEmptyObject($scope.newBenefit)) {
-                            $scope.supp.benefits.push($scope.newBenefit);
-                            $scope.newBenefit = "";
+                        if ($scope.newBenefit.description !== "") {
+                            if ($scope.supp.benefits.every($scope.notAlreadySaved)) {
+                                $scope.supp.benefits.push({description: $scope.newBenefit.description});
+                            }
+                            $scope.newBenefit.description = "";
                         }
                     };
                     $scope.setEdit = $scope.$parent.setEdit;
-
                     $scope.delete = function(item) {
                         if (!confirm("Are you sure you want to delete this item?"))
                             return;
@@ -84,26 +79,21 @@ angular.module('SuppCtrl', [])
                                 $scope.$parent.refreshList();
                             });
                     };
-
                     $scope.isNewObject = function(item) {
-                        return item._id == undefined;//if id is set, is from server.
+                        return item._id == undefined; //if id is set, is from server.
                     };
-
+                    $scope.clearWindowMessage = function() {
+                        $scope.windowMessage.text = "";
+                        $scope.$apply();
+                    };
                     $scope.save = function(item) {
-                        $scope.lower = " updtaed in dir ";
-                        $scope.object.item1 = "oh try ";
                         if ($scope.isNewObject(item)) {
                             Supps.create(item)
                                 .success(function(data) {
                                     $scope.$parent.refreshList();
                                     //not doing anything with data, b/c it should already match client side!
                                     $scope.windowMessage.text = "Item saved!";
-//                                    $scope.$apply();
-                                    setTimeout(function() {
-                                        $scope.windowMessage.text = "";
-                                        $scope.$apply();
-
-                                    }, 2500);
+                                    setTimeout($scope.clearWindowMessage, 2500);
                                 })
                                 .error(function(data) {
                                     console.log(data);
@@ -112,14 +102,10 @@ angular.module('SuppCtrl', [])
                             Supps.update(item)
                                 .success(function(data) {
                                     console.log(data);
-                                    //not doing anything with data, b/c it should already match client side!
+                                    //not doing anything witfh data, b/c it should already match client side!
                                     $scope.setEdit({});
                                     $scope.windowMessage.text = "Item updated!";
-//                                    $scope.$apply();
-                                    setTimeout(function() {
-                                        $scope.windowMessage.text = "";
-                                        $scope.$apply();
-                                    }, 2500);
+                                    setTimeout($scope.clearWindowMessage, 2500);
                                 })
                                 .error(function(data) {
                                     console.log(data);
@@ -128,9 +114,8 @@ angular.module('SuppCtrl', [])
                     };
                 },
                 scope: {
-                    windowMessage:"=",
+                    windowMessage: "=",
                     object: "=",
-                    lower: "=",
                     supp: '=supp',
                     update: "@",
                     itemToEdit: "=",
