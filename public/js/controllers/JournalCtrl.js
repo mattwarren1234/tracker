66069a3669ebd96d6dd4e88c73a15f67fe052d5f
Matt Warren <mattwarrenmail@gmail.com>
2014-08-25 22:13:18 -0700

    redid how i'm structuring the data, day of 30 is now correct

--- public/js/controllers/JournalCtrl.js
+++ public/js/controllers/JournalCtrl.js
@@ -1,47 +1,72 @@
 angular.module('JournalCtrl', [])
     .controller('JournalController', function($scope, Supps) {
-//model of already saved item
-        $scope.supps = [];
+        $scope.currentDate = new Date();
+        $scope.getSuppEffectHistory = function() {
+            //SHOULD MAKE CALL TO SERVER. ALAS, it was not meant to be for this iteration.
+            return  [
+                {date: new Date(2013, 12, 1),
+                    score: 0.2},
+                {date: new Date(2013, 11, 1),
+                    score: 0.2},
+                {date: new Date(),
+                    score: 0.2}
+            ];
+        }
+        var sameDay = function(date1, date2) {
+            try{
+                   return (date1.getFullYear() === date2.getFullYear()
+                && date1.getDate() === date2.getDate()
+                && date1.getMonth() === date2.getMonth());
+            }catch(err){
+                console.log(err);
+            }
+         
+        }
 
-        $scope.startDate = new Date();
-        var today = new Date();
-        var startDate = new Date();
-        startDate.setDate(today.getDate() - 10);
-
-        $scope.asDays = function(diff) {
-            return  Math.floor(diff / (1000 * 60 * 60 * 24));
+        $scope.onLoad = function() {
+            $scope.supp = {};
+//assuming for one right now.            
+            $scope.supp.history = $scope.getSuppEffectHistory();
+            var today = new Date();
+            $scope.journalIndex = -1;
+            for (var i = 0; i < $scope.supp.history.length; i++) {
+                console.log($scope.supp.history[i].date);
+                if (sameDay(today, $scope.supp.history[i].date)) {
+                    $scope.journalIndex = i;
+                    break;
+                }
+            }
+            if ($scope.journalIndex === -1){
+                console.log("Error! Date not found in index");
+                //add new item, i guess? and then just push stuff?
+                
+            }
         };
-        $scope.daysInJournal = $scope.asDays(today - startDate);
 
-        var supp1 = {
-            date: new Date(),
-            name: "Fish oil",
-            benefits: [
-                {description: "Reduces joint pain",
-                    score: .5},
-                {description: "Increases mental clarity",
-                    score: .2}
-            ]
-        };
+        $scope.onLoad();
+//            var supp2 = {
+//                date: new Date(),
+//                name: "vitamin d",
+//                benefits: [
+//                    {description: "Increases wakefulness",
+//                        score: .5}
+//                ]
+//            };
+//            $scope.todaySupps =
+//                {today: [supp1, supp2]};
+//            var today = new Date();
+//            var yesterday = new Date();
+//            yesterday.setDate(today.getDate() - 1);
+//            var tomorrow = new Date();
+//            tomorrow.setDate(today.getDate() + 1);
 
-        var supp2 = {
-            date: new Date(),
-            name: "vitamin d",
-            benefits: [
-                {description: "Increases wakefulness",
-                    score: .5},
-            ]
-        };
-
-        $scope.supps = [supp1, supp2];
+//        };
+//        $scope.supps = [supp1, supp2];
 
         $scope.previousDay = function() {
         };
         $scope.nextDay = function() {
         };
-//        $("#rating").raty({ starType: 'i' });
-        $('#rating').text("<p>test</p>");
-
     })
     .directive('star', function() {
         return {
