98e41e05e1e1c30f9ab4919ba9186f7504508f94
Matt Warren <mattwarrenmail@gmail.com>
2014-09-20 22:48:17 -0700

    removed unused directive

--- public/js/controllers/ResultCtrl.js
+++ public/js/controllers/ResultCtrl.js
@@ -156,84 +156,6 @@ angular.module('ResultCtrl', [])
                 })
             }}
     })
-    .directive('lineGraph', function() {
-        return{
-            restrict: 'E',
-            scope: {data: '=chartData'},
-            link: function(scope, element, attrs) {
-                var chart = d3.select(element[0]);
-                var margin = {top: 20, right: 20, bottom: 30, left: 50},
-                width = 960 - margin.left - margin.right,
-                    height = 500 - margin.top - margin.bottom;
-
-                var parseDate = d3.time.format("%d-%b-%y").parse;
-
-                var x = d3.time.scale()
-                    .range([0, width]);
-
-                var y = d3.scale.linear()
-                    .range([height, 0]);
-
-                var xAxis = d3.svg.axis()
-                    .scale(x)
-                    .orient("bottom");
-
-                var yAxis = d3.svg.axis()
-                    .scale(y)
-                    .orient("left");
-
-                var line = d3.svg.line()
-                    .x(function(d) {
-                        return x(d.date);
-                    })
-                    .y(function(d) {
-                        return y(d.close);
-                    });
-
-                var svg = d3.select("body").append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                    .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-                d3.tsv("data.tsv", function(error, data) {
-                    data.forEach(function(d) {
-                        d.date = parseDate(d.date);
-                        d.close = +d.close;
-                    });
-
-                    x.domain(d3.extent(data, function(d) {
-                        return d.date;
-                    }));
-                    y.domain(d3.extent(data, function(d) {
-                        return d.close;
-                    }));
-
-                    svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(xAxis);
-
-                    svg.append("g")
-                        .attr("class", "y axis")
-                        .call(yAxis)
-                        .append("text")
-                        .attr("transform", "rotate(-90)")
-                        .attr("y", 6)
-                        .attr("dy", ".71em")
-                        .style("text-anchor", "end")
-                        .text("Price ($)");
-
-                    svg.append("path")
-                        .datum(data)
-                        .attr("class", "line")
-                        .attr("d", line);
-                });
-
-
-            }
-        };
-    })
     .directive('multiLineGraph', function() {
         return {
             restrict: 'E',
@@ -300,7 +222,6 @@ angular.module('ResultCtrl', [])
                     y.domain([0, d3.max(benefit.scores, function(d) {
                             return d.score;
                         }) + 1]);
-                    console.log("adding id of tag" + index)
                     svg.append("path")
                         .attr("class", "line")
                         .style("stroke", function() {
